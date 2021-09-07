a4249940f79baa926d7ad7b7f1953f2648d8b91d
chiefmikey <wolfemikl@gmail.com>
2021-09-06 21:28:01 -0600

    1.0.0

---
+++ README.md
@@ -0,0 +1,39 @@
+<div width="100%" align="center">
+
+# **Mikey Pro**
+
+### [Style Guide](https://github.com/mikey-pro/style-guide) + [Theme](https://github.com/mikey-pro/theme)
+
+  <a href="https://github.com/mikey-pro">
+    <img src="mikey-pro-logo.png" style="width: 275px" alt="Mikey Pro Logo" />
+  </a>
+  <br />
+</div>
+
+## **@mikey-pro/eslint-config-babel**
+
+A preset ESLint configuration with Babel for use with the Mikey Pro style guide
+
+## Usage
+
+### Install
+
+```shell
+npm i -D @mikey-pro/eslint-config-babel
+
+yarn add -D @mikey-pro/eslint-config-babel
+```
+
+### Configure
+
+Extend to ESLint in `package.json`:
+
+```json
+{
+  "eslintConfig": {
+    "extends": [
+      "@mikey-pro/eslint-config-babel"
+    ]
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,99 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:compat/recommended',
+    'plugin:css-modules/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.md'],
+      parser: 'markdown-eslint-parser',
+      extends: ['plugin:md/recommended'],
+      rules: {
+        'md/remark': [
+          'error',
+          {
+            plugins: [
+              'preset-lint-markdown-style-guide',
+              ['lint-emphasis-marker', '_'],
+            ],
+          },
+        ],
+        'prettier/prettier': ['error', { parser: 'markdown' }],
+      },
+    },
+    {
+      files: ['*.md.json'],
+      rules: {
+        'prettier/prettier': ['error', { parser: 'json' }],
+      },
+    },
+    {
+      files: ['*.html'],
+      parser: '@html-eslint/parser',
+      extends: ['plugin:@html-eslint/recommended'],
+      rules: {
+        '@html-eslint/indent': 'off',
+        '@html-eslint/no-extra-spacing-attrs': 'off',
+        '@html-eslint/require-closing-tags': 'off',
+        'disable-autofix/@html-eslint/require-closing-tags': [
+          'error',
+          { selfClosing: 'always' },
+        ],
+        'prettier/prettier': ['error', { parser: 'html' }],
+      },
+    },
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
+    'import/extensions': 'off',
+    'prettier/prettier': ['error'],
+  },
+  plugins: [
+    'import',
+    'prettier',
+    'json-format',
+    'css-modules',
+    '@html-eslint',
+    'disable-autofix',
+  ],
+  ignorePatterns: [
+    '!.*',
+    'node_modules',
+    'vendor',
+    'dist',
+    'package-lock.json',
+  ],
+  settings: {
+    'json/sort-package-json': 'pro',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    babelOptions: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+      ],
+    }
+  }
+};
