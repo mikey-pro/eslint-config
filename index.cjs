31621c6df19d48878bd2283f1aacd45c32abd5bc
chiefmikey <wolfemikl@gmail.com>
2021-11-10 14:48:18 -0700

    Remove no for of loops rule

--- index.cjs
+++ index.cjs
@@ -81,6 +81,28 @@ module.exports = {
     es2021: true,
   },
   rules: {
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      //  {
+      //    selector: 'ForOfStatement',
+      //    message: 'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.',
+      //  },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
     camelcase: 0,
     'no-console': 'off',
     'func-names': 'off',
